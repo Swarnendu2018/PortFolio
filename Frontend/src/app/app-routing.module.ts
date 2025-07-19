a8096f1ac252c9ec6f847d0ccae73a03e16a9409
unknown <swarnenduriku2014@gmail.com>
2025-07-19 23:38:50 +0530

    base-setup

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path:'home', component:HomePageComponent },
+  { path:'**', component:PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
